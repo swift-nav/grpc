1eded5dcb3174ae4a708864166e25223b6db9cc4
Isak Tjernberg <isak.tjernberg@swift-nav.com>
2024-04-10 15:25:08 +0200

    Revert "Twice the default stack"
    This reverts commit 5c1ee9f773cbf2f84a401f64db6a1438c4951585.

--- src/core/lib/iomgr/executor/threadpool.cc
+++ src/core/lib/iomgr/executor/threadpool.cc
@@ -64,9 +64,9 @@ void ThreadPool::SharedThreadPoolConstructor() {
 
 size_t ThreadPool::DefaultStackSize() {
 #if defined(__ANDROID__) || defined(__APPLE__)
-  return 2*1952 * 1024;
+  return 1952 * 1024;
 #else
-  return 2*64 * 1024;
+  return 64 * 1024;
 #endif
 }
 
